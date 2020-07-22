3a9a1b711351c357f3be626af374b378f90c3236
Reece Mercer <reecemercer981@gmail.com>
2020-07-22 03:23:46 +0100

    Progress so far - I need to sleep

--- include/lcd.h
+++ include/lcd.h
@@ -1,8 +1,20 @@
-// instruction and control codes
+// instruction codes
 #define EIGHT_BIT_ONE_LINE_DISPLAY 0x30
-#define DISPLAY_ON_CURSOR_APPEAR 0xF
-#define ENTRY_MODE_SET 0x6
+#define DISPLAY_ON_CURSOR_APPEAR 0x0F
+#define ENTRY_MODE_SET 0x06
+#define CLEAR_DISPLAY 0x01
+#define RETURN_HOME 0x02
 
-#define RS 0x1
-#define RW 0x2
-#define E 0x4
+// my particular pins for the 3 control signals
+#define RS 0x01
+#define RW 0x02
+#define E 0x04
+
+char TEST_STRING_SINGLE_LINE[] = "Hello world!";
+
+void write_char(char c);
+void write_str(char* str);
+void toggle_clock_latch(unsigned int microsecondDelay);
+void zero_control_signals();
+void clear_display();
+void return_home();
--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,54 @@
 #include "lcd.h"
 
 
-/*
-B (digital pin 8 to 13) (only lesser 3 bits are used here)
-D (digital pins 0 to 7) (all are used as the 8 data bits: D0 - D7)
-*/
-
-
 // set RS, RW and E bits to 0
 void zero_control_signals(){
     PORTB = 0x0;
 }
 
-// toggle clock latch    
-void toggle_clock_latch(){
-    PORTB = E; 
+// clear the display of all content
+void clear_display(){
+    PORTD = CLEAR_DISPLAY;
     zero_control_signals();
 }
 
+// TODO implementing this
+// sets DDRAM address to 0. Contents of memory remain unchanged, though
+void return_home(){
+    PORTD = RETURN_HOME;
+
+}
+
+// toggle clock latch but make sure that existing signal bits are left as they are 
+void toggle_clock_latch(unsigned int microsecondDelay){
+    PORTB = PORTB | E; 
+    zero_control_signals();
+    delayMicroseconds(microsecondDelay);
+}
+
+// write a character to the display
+void write_char(char c){
+    // change the control pins to include a tied high register select bit so we can write to display RAM
+    PORTD = c;
+    PORTB = RS;
+    PORTB = PORTB | E;
+    delayMicroseconds(37);
+    zero_control_signals();
+}
+
+// write a string of characters to the display
+void write_str(char* str){
+    for(int i = 0; i < strlen(str); ++i){
+        write_char(str[i]);
+    }
+}
+
 void setup(){
+    // the datasheet has some details about voltages that I don't yet totally understand. I'm not sure how long 
+    // it takes my board to reach the required voltage to make their delay time valid, so I'll just wait for one second. 
+    // Can play with this later if required.
     delay(1000);
+
     // first set pin modes
     // 0 through 7 (data bits) set to output
     DDRD  = 0xFF;
@@ -34,18 +63,29 @@ void setup(){
     DDRB  = 0x7;
        
     // now we can initialise the display
-    PORTD = EIGHT_BIT_ONE_LINE_DISPLAY; // eight bit mode, one line display
+    // delays for these functions can be found on page 24 of the datasheet for the HD44780 chipset
+
+    // eight bit mode, one line display
+    PORTD = EIGHT_BIT_ONE_LINE_DISPLAY;
     zero_control_signals();
-    toggle_clock_latch();
-    delayMicroseconds(50);
+    toggle_clock_latch(37);
    
+    // turn on the display and make the cursor appear (with blinks because why not?)
     PORTD = DISPLAY_ON_CURSOR_APPEAR;
-    toggle_clock_latch();
-    delayMicroseconds(50);
+    toggle_clock_latch(37);
 
-    //PORTD = ENTRY_MODE_SET;
+    // set the entry mode to increment the cursor address correctly
+    PORTD = ENTRY_MODE_SET;
+    toggle_clock_latch(37);
 
-    //toggle_clock_latch();
+    write_str(TEST_STRING_SINGLE_LINE);
+
+    delay(2500);
+    clear_display();
+    toggle_clock_latch(37);
+    delay(1000);
+
+    write_str(TEST_STRING_SINGLE_LINE);
 
 }
 
