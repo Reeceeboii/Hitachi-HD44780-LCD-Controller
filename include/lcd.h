df5b228f3fe8c7137d40da52221827dbbbb9a95a
Reece Mercer <reecemercer981@gmail.com>
2020-07-24 19:36:54 +0100

    Starting to implement two line support

--- include/lcd.h
+++ include/lcd.h
@@ -1,9 +1,11 @@
 // instruction codes
 #define EIGHT_BIT_ONE_LINE_DISPLAY 0x30
+#define EIGHT_BIT_TWO_LINE_DISPLAY 0x38
 #define DISPLAY_ON_CURSOR_APPEAR 0x0F
 #define ENTRY_MODE_SET 0x06
 #define CLEAR_DISPLAY 0x01
 #define RETURN_HOME 0x02
+#define MOVE_CURSOR_TO_SECOND_LINE 0x40
 
 // my particular pins for the 3 control signals
 #define RS 0x01
@@ -13,7 +15,6 @@
 // and some other stuff
 char TEST_STRING_SINGLE_LINE[] = "Hello world!";
 // delay of 37μs to use for clock latch toggles
-// using 40 just for some leg room
 #define STANDARD_MICROSECOND_DELAY 40
 // the delay when clearing the display or returning home is a bit longer
 #define CLEAR_OR_RETURN_MICROSECOND_DELAY 1600
@@ -25,3 +26,5 @@ void zero_control_signals();
 void clear_display();
 void return_home();
 void initialise_eight_bit_one_line();
+void initialise_eight_bit_two_line();
+void move_cursor_to_new_line();
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,13 @@ void return_home(){
     toggle_clock_latch(CLEAR_OR_RETURN_MICROSECOND_DELAY);
 }
 
+// On 2 line mode, moves the cursor to the second line
+void move_cursor_to_new_line(){
+    PORTD = MOVE_CURSOR_TO_SECOND_LINE;
+    PORTB = 0;
+    toggle_clock_latch(STANDARD_MICROSECOND_DELAY);
+}
+
 // Toggle the chipset's clock latch and wait a given number of microseconds.
 // As we don't want to affect any other control bits before an instruction is sent, we can just simply
 // toggle the control latch bit on its own.
@@ -47,7 +54,7 @@ void write_str(char* str){
     }
 }
 
-// Carry out all of the functions required to boot theb display into eight bit, one line display mode.
+// Carry out all of the functions required to boot the display into eight bit, one line display mode.
 // Delays for these functions can be found on page 24 of the datasheet for the HD44780 chipset.
 // Most of them use a seemingly standard 37μs delay
 void initialise_eight_bit_one_line(){
@@ -63,7 +70,22 @@ void initialise_eight_bit_one_line(){
 
     // set the entry mode to increment the cursor address correctly
     PORTD = ENTRY_MODE_SET;
-    toggle_clock_latch(37);
+    toggle_clock_latch(STANDARD_MICROSECOND_DELAY);
+}
+
+// Same as the above function, but for two line mode
+void initialise_eight_bit_two_line(){
+    PORTB = 0;
+
+    PORTD = EIGHT_BIT_TWO_LINE_DISPLAY;
+    toggle_clock_latch(STANDARD_MICROSECOND_DELAY);
+
+    PORTD = DISPLAY_ON_CURSOR_APPEAR;
+    toggle_clock_latch(STANDARD_MICROSECOND_DELAY);
+
+    PORTD = ENTRY_MODE_SET;
+    toggle_clock_latch(STANDARD_MICROSECOND_DELAY);
+
 }
 
 // Initialise the LCD
@@ -71,16 +93,19 @@ void setup(){
     // Give the display sufficient time to carry out its internal reset function
     delay(1000);
 
-    // Before we can do anything, we set and data direction registers to be correct
+    // Before we can do anything, we set the data direction registers to be correct
     // 0 through 7 (data bits) set to output
     DDRD  = 0xFF;
     // first 3 pins (of PORTB) used for control signals
     DDRB  = 0x07;
        
     // now we can initialise the display
-    initialise_eight_bit_one_line();
+    //initialise_eight_bit_one_line();
+    initialise_eight_bit_two_line();
     clear_display();
 
+    write_str(TEST_STRING_SINGLE_LINE);
+    move_cursor_to_new_line();
     write_str(TEST_STRING_SINGLE_LINE);
 }
 
